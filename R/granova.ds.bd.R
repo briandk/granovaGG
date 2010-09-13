dc682c90f7f0b2d6ab31dbaa6f157c91f7258469
Brian Danielak <briandk@umd.edu>
2010-09-13 01:28:46 -0400

    Add 95% Confidence Band. But, I don't think it's computing properly. Still have to add shadows to point trails

--- R/granova.ds.bd.R
+++ R/granova.ds.bd.R
@@ -15,14 +15,29 @@ ttest<-t.test(dd$xvals, dd$yvals,
               paired     = TRUE,
               conf.level = 0.95)
 
-confidenceInterval <- (ttest$conf.int)
+
+ttest
+cint <- (ttest$conf.int)
+
 
 # Setting the graphicalbounds
 extrema <- c(range(dd$xvals), range(dd$yvals))
 offset  <- (max(extrema) - min(extrema)) / 10
 bounds  <- c(min(extrema) - 5*offset, max(extrema) + offset)
-perpendicularIntercept <- 2*(min(dd$yvals)) - offset
+(perpendicularIntercept <- 2*(min(dd$yvals)) - offset)
 shadowOffset <- offset/4
+
+# Computing the Confidence Interval shadow               
+
+cxstart    = perpendicularIntercept/2 + cint[1]/sqrt(2)
+cystart    = perpendicularIntercept/2 - cint[1]/sqrt(2)
+cxend      = perpendicularIntercept/2 + cint[2]/sqrt(2)
+cyend      = perpendicularIntercept/2 - cint[2]/sqrt(2)
+
+cxstart
+cystart
+cxend  
+cyend  
 
 # Computing point shadows
 
@@ -78,15 +93,34 @@ p <- p + geom_abline(intercept = mean(dd$yvals) - mean(dd$xvals),
                      linetype  = 2)
                      
 # Plotting point shadows
-p <- p + geom_point(data = ddshadow, color = "blue", size = I(3))
+p <- p + geom_point(
+           data  = ddshadow, 
+           color = "steelblue", 
+           size  = I(3),
+           alpha = I(1/2) 
+        )
 
-# Plotting the Confidence Interval                  
-
+# Plotting the 95% Confidence band
+p <- p + geom_segment(
+           aes(
+                x    = cxstart - offset/6,
+                y    = cystart - offset/6,
+                xend = cxend   - offset/6,
+                yend = cyend   - offset/6,
+                
+           ), size  = I(2),
+              color = "darkgreen",
+              alpha = I(1)
+         )
+     
+         
+p + opts(title = "Dependent Sample Scatterplot for pair65 data")  
 # Removing the gridlines and background
 p +
   opts(panel.grid.major = theme_blank()) +  
   opts(panel.grid.minor = theme_blank()) +
   opts(panel.background = theme_blank()) + 
   opts(axis.line = theme_segment()) +
-  opts(title = "Dependent Sample Scatterplot for pair65 data")
+  opts(title = "Dependent Sample Scatterplot for pair65 data")  
+  
                                     
