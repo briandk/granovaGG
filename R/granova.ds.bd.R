9a66d98552c25a25aadd5ecd0ea1807c8c40da71
Brian Danielak <briandk@umd.edu>
2010-10-11 18:05:58 -0400

    Wrap plots in granova.ds.bd function; add testplots.pdf to .gitignore

--- R/granova.ds.bd.R
+++ R/granova.ds.bd.R
@@ -7,136 +7,142 @@ library(DAAG)
 data(pair65)
 str(pair65)
 
-dd <- data.frame(
-  xvals  = pair65$heated, 
-  yvals  = pair65$ambient,
-  effect = pair65$heated - pair65$ambient)
-str(dd)
+# Defining the Function
 
-# Computing Some Statistics
-effectQuantiles <- quantile(dd$effect, probs = c(0, 0.025, 0.5, 0.975, 1))
+granova.ds.bd <- function(data = pair65) {
 
-dsttest <- t.test(dd$yvals, dd$xvals, 
-                 paired     = TRUE,
-                 conf.level = 0.95)
+  dd <- data.frame(
+    xvals  = data$heated, 
+    yvals  = data$ambient,
+    effect = data$heated - data$ambient)
+  str(dd)
 
-(meanTreatmentEffect  <- dsttest$estimate)
-(upperTreatmentEffect <- dsttest$conf.int[1])
-(lowerTreatmentEffect <- dsttest$conf.int[2])
+  # Computing Some Statistics
+  effectQuantiles <- quantile(dd$effect, probs = c(0, 0.025, 0.5, 0.975, 1))
 
-lm1 <- lm(yvals ~ xvals, data = dd)
-summary(lm1)
-meanTreatmentEffect
+  dsttest <- t.test(dd$yvals, dd$xvals, 
+                   paired     = TRUE,
+                   conf.level = 0.95)
 
-# Setting the graphicalbounds
-extrema  <- c(range(dd$xvals), range(dd$yvals))
-offset   <- (max(extrema) - min(extrema)) / 10
-bounds   <- c(min(extrema) - 5*offset, max(extrema) + offset)
-(perpendicularIntercept <- 2*(min(dd$yvals)) - offset)
-shadowOffset <- offset/6
+  (meanTreatmentEffect  <- dsttest$estimate)
+  (upperTreatmentEffect <- dsttest$conf.int[1])
+  (lowerTreatmentEffect <- dsttest$conf.int[2])
 
-# Computing point shadows
-xshadow <- (((dd$xvals - dd$yvals) + perpendicularIntercept) /2) + shadowOffset
-yshadow <- (xshadow) + (dd$yvals - dd$xvals)
+  lm1 <- lm(yvals ~ xvals, data = dd)
+  summary(lm1)
+  meanTreatmentEffect
 
-xshadow
-yshadow
-# I have to name the resultant dataframe variables as "xvals" and "yvals" so
-# that the subsequent geom_point(data = ddshadow) can inherit the dd dataframe
-# column names and plot correctly (Wickham, ggplot2 book, p. 63)
-ddshadow <- data.frame(xvals = xshadow, yvals = yshadow)
-ddshadow
+  # Setting the graphicalbounds
+  extrema  <- c(range(dd$xvals), range(dd$yvals))
+  offset   <- (max(extrema) - min(extrema)) / 10
+  bounds   <- c(min(extrema) - 5*offset, max(extrema) + offset)
+  (perpendicularIntercept <- 2*(min(dd$yvals)) - offset)
+  shadowOffset <- offset/6
 
-# Plotting the standard granova plot
-granova.ds(pair65,
-  main = "Dependent sample assessment plot for pair65 data, n = 9")
+  # Computing point shadows
+  xshadow <- (((dd$xvals - dd$yvals) + perpendicularIntercept) /2) + shadowOffset
+  yshadow <- (xshadow) + (dd$yvals - dd$xvals)
+
+  xshadow
+  yshadow
+  # I have to name the resultant dataframe variables as "xvals" and "yvals" so
+  # that the subsequent geom_point(data = ddshadow) can inherit the dd dataframe
+  # column names and plot correctly (Wickham, ggplot2 book, p. 63)
+  ddshadow <- data.frame(xvals = xshadow, yvals = yshadow)
+  ddshadow
+
+  # Plotting the standard granova plot
+  granova.ds(pair65,
+    main = "Dependent sample assessment plot for pair65 data, n = 9")
   
-## Trying to get the same plot in ggplot2
-p <- ggplot(aes(x = xvals, y = yvals), 
-              data = dd)
+  ## Trying to get the same plot in ggplot2
+  p <- ggplot(aes(x = xvals, y = yvals), 
+                data = dd)
               
-p <- p + geom_point(size = I(3)) + xlim(bounds) + ylim(bounds)
+  p <- p + geom_point(size = I(3)) + xlim(bounds) + ylim(bounds)
 
-# Adding the y=x line
-p <- p + geom_abline(slope = 1, intercept = 0)
+  # Adding the y=x line
+  p <- p + geom_abline(slope = 1, intercept = 0)
 
-# Forcing coordinates to be equal
-p <- p + coord_equal()
+  # Forcing coordinates to be equal
+  p <- p + coord_equal()
 
-# Adding a rugplot
-p <- p + geom_rug(alpha = I(2/3))
+  # Adding a rugplot
+  p <- p + geom_rug(alpha = I(2/3))
 
-# Adding a perpendicular cross-section
-p <- p + geom_abline(intercept = perpendicularIntercept, 
-                     slope     = -1)
+  # Adding a perpendicular cross-section
+  p <- p + geom_abline(intercept = perpendicularIntercept, 
+                       slope     = -1)
                 
-# Adding group mean lines
-p <- p + geom_hline(yintercept = mean(dd$yvals), 
-                    colour     = "red",
-                    alpha      = 1/2,
-                    linetype   = 3)
+  # Adding group mean lines
+  p <- p + geom_hline(yintercept = mean(dd$yvals), 
+                      colour     = "red",
+                      alpha      = 1/2,
+                      linetype   = 3)
                     
-p <- p + geom_vline(xintercept = mean(dd$xvals), 
-                    colour     = "red",
-                    alpha      = 1/2,
-                    linetype   = 3) 
+  p <- p + geom_vline(xintercept = mean(dd$xvals), 
+                      colour     = "red",
+                      alpha      = 1/2,
+                      linetype   = 3) 
 
-# Adding the treatment effect line
-p <- p + geom_abline(intercept = meanTreatmentEffect,
-                     slope     = 1,
-                     color     = "red",
-                     alpha     = 1,
-                     linetype  = 2)
+  # Adding the treatment effect line
+  p <- p + geom_abline(intercept = meanTreatmentEffect,
+                       slope     = 1,
+                       color     = "red",
+                       alpha     = 1,
+                       linetype  = 2)
                      
-# Plotting point shadows
-p <- p + geom_point(
-           data  = ddshadow, 
-           color = "black", 
-           size  = I(3),
-           alpha = I(1/2) 
-         )
+  # Plotting point shadows
+  p <- p + geom_point(
+             data  = ddshadow, 
+             color = "black", 
+             size  = I(3),
+             alpha = I(1/2) 
+           )
 
-# Plotting the point trails
-p <- p + geom_segment(
-           aes(
-             x = dd$xvals,
-             y = dd$yvals,
-             xend = ddshadow$xvals,
-             yend = ddshadow$yvals           
+  # Plotting the point trails
+  p <- p + geom_segment(
+             aes(
+               x = dd$xvals,
+               y = dd$yvals,
+               xend = ddshadow$xvals,
+               yend = ddshadow$yvals           
            
-           ), size     = I(1),
-              color    = "black",
-              linetype = 3,
-              alpha    = I(1/4), 
-         )
+             ), size     = I(1),
+                color    = "black",
+                linetype = 3,
+                alpha    = I(1/4), 
+           )
 
 
-# Plotting the 95% Confidence band
-p <- p + geom_segment(
-           aes(
-             x    = ((perpendicularIntercept - lowerTreatmentEffect) / 2) 
-                     - shadowOffset,
-             y    = ((perpendicularIntercept + lowerTreatmentEffect) / 2) 
-                     - shadowOffset,
-             xend = ((perpendicularIntercept - upperTreatmentEffect) / 2) 
-                     - shadowOffset,
-             yend = ((perpendicularIntercept + upperTreatmentEffect) / 2) 
-                     - shadowOffset
+  # Plotting the 95% Confidence band
+  p <- p + geom_segment(
+             aes(
+               x    = ((perpendicularIntercept - lowerTreatmentEffect) / 2) 
+                       - shadowOffset,
+               y    = ((perpendicularIntercept + lowerTreatmentEffect) / 2) 
+                       - shadowOffset,
+               xend = ((perpendicularIntercept - upperTreatmentEffect) / 2) 
+                       - shadowOffset,
+               yend = ((perpendicularIntercept + upperTreatmentEffect) / 2) 
+                       - shadowOffset
                 
-           ), size  = I(2),
-              color = "darkgreen",
-              alpha = I(1),
+             ), size  = I(2),
+                color = "darkgreen",
+                alpha = I(1),
              
-         )
+           )
      
-p
+  p
 
-# Removing the gridlines and background
-p +
-  opts(panel.grid.major = theme_blank()) +  
-  opts(panel.grid.minor = theme_blank()) +
-  opts(panel.background = theme_blank()) + 
-  opts(axis.line = theme_segment()) +
-  opts(title = "Dependent Sample Scatterplot for pair65 data")  
-  
+  # Removing the gridlines and background
+  p +
+    opts(panel.grid.major = theme_blank()) +  
+    opts(panel.grid.minor = theme_blank()) +
+    opts(panel.background = theme_blank()) + 
+    opts(axis.line = theme_segment()) +
+    opts(title = "Dependent Sample Scatterplot for pair65 data")  
+
+}  
                                     
+granova.ds.bd
