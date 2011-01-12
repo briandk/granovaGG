4c0d292907fc519ec22fe95d441a57fab2770efc
Brian Danielak <briandk@umd.edu>
2011-01-12 15:51:08 -0700

    Experiment with some list-generating code on the Scratchpad.

--- R/Scratchpad.R
+++ R/Scratchpad.R
@@ -1,66 +1,20 @@
-## Required Libraries
-library(ggplot2)
-library(DAAG) # contains the pair65 data
+x <- 1
+y <- 2
+z <- 3
 
-## Loading in the data
-data(pair65)
-str(pair65)
+ll <- list(x, y, z)
 
-## Defining the Function
+str(ll)
 
-granova.ds.bd <- function(
-                   data                      = pair65, 
-                   southwestPlotOffsetFactor = 0.4,
-                   northeastPlotOffsetFactor = 0.5,
-                   plotTitle                 = "Dependent Sample Scatterplot",
-                   tTestConfidenceLevel      = 0.95,
-                   produceBlankPlotObject    = TRUE
-) 
+dd <- data.frame(
+  xvals = rnorm(n = 25),
+  yvals = rnorm(n = 25)  
+)
 
-{
-  dd <- data.frame(
-          xvals  = data[ , 1], 
-          yvals  = data[ , 2],
-          effect = data[ , 2]  - data[ , 1]
-        )  
+dd <- structure(dd, class = "granovaData")
+ll <- list(graphicalParameters = ll, granovaData = dd)
 
-  # We're going to build the plot in several pieces. First, we compute
-  # statistics on the data passed in, and use them to define square graphical
-  # bounds for the viewing window. Then, we use grammar to build the plot layer
-  # by layer. Because of the way ggplot2 creates plot objects, layers can be
-  # added to a plot p simply by calling "p <- p + newLayer", so for now you'll
-  # see that structure of code throughout.
-    
-  ## Computing t-test Statistics for the Confidence Band and Mean Difference
-  performDependentSampleTtest <- function (xValues, yValues, confidenceLevel) {
-    return (
-      t.test( 
-              xValues, 
-              yValues, 
-              paired     = TRUE,
-              conf.level = confidenceLevel
-      )
-    )
-  }
-  
-  dependentSampleTtestStatistics <- performDependentSampleTtest(
-                                      dd$xvals,
-                                      dd$yvals,
-                                      confidenceLevel = tTestConfidenceLevel
-  )
+str(ll)
 
-  getTreatmentEffectQuantiles <- function (tTestStatistics) {
-    effectQuantiles <- data.frame(
-      lowerTreatmentEffect = as.numeric(tTestStatistics$conf.int[2]),
-      meanTreatmentEffect  = as.numeric(tTestStatistics$estimate),
-      upperTreatmentEffect = as.numeric(tTestStatistics$conf.int[1])
-    )  
-    return(effectQuantiles)
-  }
-  
-  treatmentEffects <- getTreatmentEffectQuantiles(dependentSampleTtestStatistics)
-  print((treatmentEffects$meanTreatmentEffect))
-  print(treatmentEffects)
-}
+ll$granovaData$xvals
 
-granova.ds.bd(pair65)
