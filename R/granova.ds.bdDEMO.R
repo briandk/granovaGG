82decfbcccdecf3311e26db06743f7c54c76cba9
Brian Danielak <briandk@umd.edu>
2011-01-21 23:55:30 -0500

    DEMO code now plots classic granova.ds plots for comparison. Aligned assignments in theme-defaults.R. Specified the parameters for theme_granova().

--- R/granova.ds.bdDEMO.R
+++ R/granova.ds.bdDEMO.R
@@ -15,16 +15,21 @@ tobacco  <- loadGranovaData("../data/tobacco.csv")
 anorexia <- loadGranovaData("../data/anorexia.csv")
 lead     <- loadGranovaData("../data/lead.csv")
 
+pdf("PruzekTestPlots.pdf", onefile = TRUE)
+
+granova.ds(lead[ , c(1,2)])
+granova.ds(tobacco[ , c(1,2)])
+granova.ds(anorexia[ , c(1,2)])
+
 granova.ds.bd(
   data               = lead, 
   conf.level         = 0.95, 
-  plotTitle          = "Lead Levels in the Blood of Children",
+  plotTitle          = "Lead Levels in the Blood of Children (theme_granova)",
 ) 
 
 p <- granova.ds.bd(
   data               = tobacco, 
-  conf.level         = 0.95, 
-  plotTitle          = "Effect of Preparation on Lesions",
+  plotTitle          = "Effect of Preparation on Lesions (theme_granova)",
 ) 
 
 caseLabels <- geom_text(
@@ -39,17 +44,18 @@ p + caseLabels
 
 granova.ds.bd(
   data               = anorexia, 
-  conf.level         = 0.95, 
-  plotTitle          = "Effect of Therapy on Anorexic Patients",
+  plotTitle          = "Effect of Therapy on Anorexic Patients (theme_granova)",
   plotTheme          = theme_granova()
 ) 
 
 granova.ds.bd(
   data               = anorexia, 
   conf.level         = 0.95, 
-  plotTitle          = "Effect of Therapy on Anorexic Patients",
+  plotTitle          = "Effect of Therapy on Anorexic Patients (theme_gray)",
   plotTheme          = theme_gray()
-)
+) # Same plot as above, but with the default ggplot2 theme applied.
+
+dev.off()
 
 
 
