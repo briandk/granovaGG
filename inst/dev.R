d909c7896a45d90f9e1fc7974f06c681c6f7041c
Brian Danielak <briandk@umd.edu>
2011-01-22 17:51:24 -0500

    Modified dev.R to search through default R Library locations when finding existing versions of granova.

--- inst/dev.R
+++ inst/dev.R
@@ -1,25 +1,31 @@
-isGranovaInstalled <- function () {
-  return( "granova" %in% .packages(all.available = TRUE))  
+isGranovaInstalled <- function (RLibraryLocations) {
+  return( "granova" %in% .packages(all.available = TRUE, lib.loc = RLibraryLocations))  
 }
 
 isGranovaAttached <- function () {
   return( ("package:granova" %in% search()))
 }
 
-removeCurrentVersionOfGranova <- function () {
-  if ( isGranovaInstalled() ) {
+removeCurrentVersionOfGranova <- function (RLibraryLocations) {
+  if ( isGranovaInstalled(RLibraryLocations) ) {
     
     if (isGranovaAttached()) {
       print("Detaching granova package")
       detach(package:granova)
     }  
     
-    remove.packages("granova")
+    print("Removing granova package")
+    remove.packages("granova", lib = RLibraryLocations)
   }
 }
 
 installGranovaDev <- function () {
-  removeCurrentVersionOfGranova()
+  RLibraryLocations <- c(
+    "~/Library/R/2.12/library",
+    "/Library/Frameworks/R.framework/Versions/2.12/Resources/library"
+  )
+  
+  removeCurrentVersionOfGranova(RLibraryLocations)
   install.packages(
     pkgs    = '~/Dropbox/Brian-Wil/programming/granova/', 
     repos   = NULL, 
@@ -27,5 +33,7 @@ installGranovaDev <- function () {
   )
 }
 
+
 installGranovaDev()
+library(granova)
 demo("granova.ds.bdDEMO")
