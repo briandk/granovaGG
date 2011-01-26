c7a27da94894223fe906f2e6237beeba51d6768c
Brian Danielak <briandk@umd.edu>
2011-01-26 16:49:37 -0500

    Created convertToDataFrame(). Added getSquareDataRange() to list of globally available functions in granova.R.

--- R/granova.1w.ggplot.R
+++ R/granova.1w.ggplot.R
@@ -1,9 +1,15 @@
 granova.1w.ggplot <- function( data  = NULL,
-                               group = NULL
-  
+                               group = NULL,
+                               resid = TRUE
                      ) 
 {
-
+  convertToDataFrame <- function () {
+    if (is.data.frame(data)) {
+      return(data)  
+    }
+    
+    if (is.matrix(data))
+  }
 
 }
 
--- R/granova.R
+++ R/granova.R
@@ -1,2 +1,6 @@
-
-
+getSquareDataRange <- function (data) {
+  .aggregateDataRange  <- c(range(getXs(dsp$data)), range(getYs(dsp$data)))
+  .extrema             <- c(max(.aggregateDataRange), min(.aggregateDataRange))    
+  .squareDataRange     <- max(.extrema) - min(.extrema)
+  
+}
