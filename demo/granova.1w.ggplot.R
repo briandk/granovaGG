2fc156d58b2f922fe501dcc0403cc0b2b08b93d8
Brian Danielak <briandk@umd.edu>
2011-01-30 17:03:47 -0500

    theme_granova is now based off of theme_bw(). Tweaked appearance of major gridlines. Added comparison theme plots to granova.1w.ggplot() demo code.

--- demo/granova.1w.ggplot.R
+++ demo/granova.1w.ggplot.R
@@ -2,5 +2,8 @@ data(poison)
 ##Note violation of constant variance across groups in following graphic.
 granova.1w(poison$RateSurvTime, group = poison$Group, resid = TRUE)
 granova.1w.ggplot(poison$RateSurvTime, group = poison$Group, resid = TRUE)
+granova.1w.ggplot(poison$RateSurvTime, group = poison$Group, resid = TRUE, main = "theme_bw()") + theme_bw()
+qplot(x = mpg, y = hp, data = mtcars) + theme_bw()
+qplot(x = mpg, y = hp, data = mtcars)
 ##RateSurvTime = SurvTime^-1
 # granova.1w.ggplot(poison)
