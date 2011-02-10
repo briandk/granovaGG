419c404742a6773b8a1d22e5eff272b27eee36f8
Brian Danielak <briandk@umd.edu>
2011-02-09 21:42:04 -0500

    Added an additional example to the granova.1w demo that highlights non-45-degree angle datasets for the group mean lines.

--- demo/granova.1w.ggplot.R
+++ demo/granova.1w.ggplot.R
@@ -3,5 +3,7 @@ data(poison)
 granova.1w(poison$RateSurvTime, group = poison$Group, resid = TRUE)
 granova.1w.ggplot(poison$RateSurvTime, group = poison$Group, resid = TRUE)
 
+granova.1w(mtcars$hp, group = mtcars$cyl, resid = TRUE)
+granova.1w.ggplot(mtcars$hp, group = mtcars$cyl, resid = TRUE)
 ##RateSurvTime = SurvTime^-1
 # granova.1w.ggplot(poison)
