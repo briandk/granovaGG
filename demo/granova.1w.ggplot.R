5c200ae99bf481aa081d19fc80454678bf4c7fcf
Brian Danielak <briandk@umd.edu>
2011-02-11 17:46:54 -0500

    Added demonstration plot for showing data with unequal variances.

--- demo/granova.1w.ggplot.R
+++ demo/granova.1w.ggplot.R
@@ -1,5 +1,8 @@
 data(poison)
 ##Note violation of constant variance across groups in following graphic.
+granova.1w(poison$SurvTime, group = poison$Group, resid = TRUE)
+granova.1w.ggplot(poison$SurvTime, group = poison$Group, resid = TRUE)
+
 granova.1w(poison$RateSurvTime, group = poison$Group, resid = TRUE)
 granova.1w.ggplot(poison$RateSurvTime, group = poison$Group, resid = TRUE)
 
