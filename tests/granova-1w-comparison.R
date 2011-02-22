312b973b257605d3420b5fea0ee59159de366b92
Brian Danielak <briandk@umd.edu>
2011-02-22 11:32:38 -0500

    Modified granova-1w-comparison to produce just one set of poison data output, as well as one set of handmade data output.

--- tests/granova-1w-comparison.R
+++ tests/granova-1w-comparison.R
@@ -1,16 +1,22 @@
 data(poison)
 ##Note violation of constant variance across groups in following graphic.
-granova.1w(poison$SurvTime, group = poison$Group, resid = TRUE)
-granova.1w.ggplot(poison$SurvTime, group = poison$Group, resid = TRUE)
-
+# granova.1w(poison$SurvTime, group = poison$Group, resid = TRUE)
+# granova.1w.ggplot(poison$SurvTime, group = poison$Group, resid = TRUE)
+# 
 granova.1w(poison$RateSurvTime, group        = poison$Group, resid = TRUE)
 granova.1w.ggplot(poison$RateSurvTime, group = poison$Group, resid = TRUE)
+# 
+# granova.1w(mtcars$hp, group        = mtcars$cyl, resid = TRUE, v.rng = 1)
+# granova.1w.ggplot(mtcars$hp, group = mtcars$cyl, resid = TRUE, v.rng = 1)
+# 
+# granova.1w(mtcars$hp, group        = mtcars$gear, resid = TRUE, v.rng = 20)
+# granova.1w.ggplot(mtcars$hp, group = mtcars$gear, resid = TRUE, v.rng = 20)
 
-granova.1w(mtcars$hp, group        = mtcars$cyl, resid = TRUE, v.rng = 1)
-granova.1w.ggplot(mtcars$hp, group = mtcars$cyl, resid = TRUE, v.rng = 1)
+x <- rnorm(n = 144)
+y <- rep(c("f", "j", "t", "q", "b", "y"), times = 24)
 
-granova.1w(mtcars$hp, group        = mtcars$gear, resid = TRUE, v.rng = 20)
-granova.1w.ggplot(mtcars$hp, group = mtcars$gear, resid = TRUE, v.rng = 20)
+dd <- data.frame(x, y)
+
 
 ##RateSurvTime = SurvTime^-1
 # granova.1w.ggplot(poison)
