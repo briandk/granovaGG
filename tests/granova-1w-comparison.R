ce0569548fa15cfa4d98c7d7ebaea8cefa6eb2b7
Brian Danielak <briandk@umd.edu>
2011-02-22 16:38:48 -0500

    Created a test dataframe where the F-statistic for ANOVA is purposefully < 1.

--- tests/granova-1w-comparison.R
+++ tests/granova-1w-comparison.R
@@ -12,11 +12,12 @@ granova.1w.ggplot(poison$RateSurvTime, group = poison$Group, resid = TRUE)
 # granova.1w(mtcars$hp, group        = mtcars$gear, resid = TRUE, v.rng = 20)
 # granova.1w.ggplot(mtcars$hp, group = mtcars$gear, resid = TRUE, v.rng = 20)
 
-x <- rnorm(n = 144)
-y <- rep(c("f", "j", "t", "q", "b", "y"), times = 24)
+set.seed(1001)
+x <- rnorm(n = 64)
+y <- rep(c("a", "b", "c", "d", "e", "f", "g", "h"), times = 8)
 
 dd <- data.frame(x, y)
 
-
+granova.1w.ggplot(dd$x, group = dd$y, resid = TRUE)
 ##RateSurvTime = SurvTime^-1
 # granova.1w.ggplot(poison)
