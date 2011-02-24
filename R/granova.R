934bce87105ece4d78888e9eb7c5fd5f5f3376ae
Brian Danielak <briandk@umd.edu>
2011-02-24 01:53:08 -0500

    theme_granova_1w() is once again based explicitly off of theme_bw().

--- R/granova.R
+++ R/granova.R
@@ -1,5 +1,5 @@
 theme_granova_1w <- function(base_size = 12) {
-  theme_granova_1w                  <- theme_gray()
+  theme_granova_1w                  <- theme_bw()
   theme_granova_1w$axis.text.x      <- theme_text(size = 8 , lineheight = 0.9, vjust = 1, angle = 90, colour = "grey50")
   theme_granova_1w$axis.text.y      <- theme_text(size = 8 , lineheight = 0.9, hjust = 1, colour = "grey50")
   theme_granova_1w$axis.title.x     <- theme_text(size = 8, vjust = 2, colour = "grey20")
@@ -10,7 +10,7 @@ theme_granova_1w <- function(base_size = 12) {
   
   theme_granova_1w$panel.border     <- theme_blank()
   theme_granova_1w$panel.grid.minor <- theme_line(colour = NA, size = 0.25)
-  theme_granova_1w$panel.grid.major <- theme_line(colour = "white", size = 0.1)
+  theme_granova_1w$panel.grid.major <- theme_line(colour = "grey90", size = 0.1)
   theme_granova_1w$plot.title       <- theme_text(face = "bold", size = 10, vjust = -1)
   return(theme_granova_1w)
 }
