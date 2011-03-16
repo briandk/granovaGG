c9aa857e0a44c6842c2c355e85e1b3ac20c9d2da
Brian Danielak <briandk@umd.edu>
2011-03-16 16:59:57 -0400

    Added explicit calls to theme_granova_1w in granova-1w-comparison.R.

--- tests/granova-1w-comparison.R
+++ tests/granova-1w-comparison.R
@@ -3,14 +3,14 @@ data(poison)
 # granova.1w(poison$SurvTime, group = poison$Group, resid = TRUE)
 # granova.1w.ggplot(poison$SurvTime, group = poison$Group, resid = TRUE)
 # 
-granova.1w(poison$RateSurvTime, group        = poison$Group, resid = TRUE)
-granova.1w.ggplot(poison$RateSurvTime, group = poison$Group, resid = TRUE, plot.theme = "theme_granova_1w_gray")
+granova.1w.ggplot(poison$SurvTime, group        = poison$Group, resid = TRUE, plot.theme = "theme_granova_1w")
+granova.1w.ggplot(poison$RateSurvTime, group = poison$Group, resid = TRUE, plot.theme = "theme_granova_1w")
 # 
 # granova.1w(mtcars$hp, group        = mtcars$cyl, resid = TRUE, v.rng = 1)
-# granova.1w.ggplot(mtcars$hp, group = mtcars$cyl, resid = TRUE, v.rng = 1)
+granova.1w.ggplot(mtcars$hp, group = mtcars$cyl, resid = TRUE, v.rng = 1, plot.theme = "theme_granova_1w")
 # 
 # granova.1w(mtcars$hp, group        = mtcars$gear, resid = TRUE, v.rng = 20)
-# granova.1w.ggplot(mtcars$hp, group = mtcars$gear, resid = TRUE, v.rng = 20)
+granova.1w.ggplot(mtcars$hp, group = mtcars$gear, resid = TRUE, v.rng = 20, plot.theme = "theme_granova_1w")
 
 set.seed(1001)
 x <- rnorm(n = 64)
@@ -18,6 +18,6 @@ y <- rep(c("a", "b", "c", "d", "e", "f", "g", "h"), times = 8)
 
 dd <- data.frame(x, y)
 
-granova.1w.ggplot(dd$x, group = dd$y, resid = TRUE)
+granova.1w.ggplot(dd$x, group = dd$y, resid = TRUE, plot.theme = "theme_granova_1w")
 ##RateSurvTime = SurvTime^-1
 # granova.1w.ggplot(poison)
