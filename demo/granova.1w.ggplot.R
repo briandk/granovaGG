b15ce2226a10aa2d45fa746d4ba5d035eaf68814
Brian Danielak <briandk@umd.edu>
2011-03-30 18:34:37 -0400

    Formatting changes granova.1w.ggplot demo script.

--- demo/granova.1w.ggplot.R
+++ demo/granova.1w.ggplot.R
@@ -1,9 +1,25 @@
 data(poison)
 ##Note violation of constant variance across groups in following graphic.
-granova.1w.ggplot(poison$SurvTime, group = poison$Group, resid = TRUE)
-granova.1w.ggplot(poison$RateSurvTime, group        = poison$Group, resid = TRUE, v.rng = 0)
-granova.1w.ggplot(mtcars$hp, group        = mtcars$cyl, resid = TRUE, h.rng = 1)
-granova.1w.ggplot(mtcars$hp, group        = mtcars$gear, resid = TRUE, h.rng = 20)
+granova.1w.ggplot(poison$SurvTime,     
+  group = poison$Group, 
+  resid = TRUE
+)
 
-##RateSurvTime = SurvTime^-1
-# granova.1w.ggplot(poison)
+granova.1w.ggplot(poison$RateSurvTime, 
+  group = poison$Group, 
+  resid = TRUE, 
+  v.rng = 0
+)
+
+granova.1w.ggplot(mtcars$hp, 
+  group = mtcars$cyl, 
+  resid = TRUE, 
+  h.rng = 1
+)
+
+granova.1w.ggplot(mtcars$hp, 
+  group = mtcars$gear, 
+  resid = TRUE, 
+  h.rng = 20
+)
+
