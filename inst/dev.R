48bd63b602fc3be574fe8b42f985b0cf86691e41
Brian Danielak <briandk@umd.edu>
2011-03-30 17:56:58 -0400

    Changed output of dev.R from granova-1w-comparison to demo of granova.1w.ggplot().

--- inst/dev.R
+++ inst/dev.R
@@ -32,5 +32,5 @@ library(granova)
 
 # trace(granova.1w.ggplot, browser)
 # pdf(file = "../temp/granova.1w.ggplot.pdf", onefile = TRUE)
-source(file = "../tests/granova-1w-comparison.R", chdir = TRUE, echo = TRUE)
+demo(granova.1w.ggplot)
 #dev.off()
