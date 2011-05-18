4530979a58075691f390934a7d468db06af8580c
Brian Danielak <briandk@umd.edu>
2011-05-17 18:55:45 -0700

    Added runUnitTests() function to dev.R

--- inst/dev.R
+++ inst/dev.R
@@ -26,11 +26,19 @@ installGranovaDev <- function() {
   )
 }
 
-removeCurrentVersionOfGranova()
-installGranovaDev()
-library(granova)
+runUnitTests <- function (pathToTestFile) {
+  library(testthat)
+  test_file(paste(pathToTestFile))
+}
+
+runUnitTests("tests/01.R")
+
+
+# removeCurrentVersionOfGranova()
+# installGranovaDev()
+# library(granova)
 
 # trace(granova.1w.ggplot, browser)
 # pdf(file = "../temp/granova.1w.ggplot.pdf", onefile = TRUE)
-demo(granova.1w.ggplot)
+
 #dev.off()
