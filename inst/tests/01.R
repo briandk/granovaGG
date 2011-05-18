4d2745da491c8b31ad0161ad273d3f567d3a3a37
Brian Danielak <briandk@umd.edu>
2011-05-17 18:55:10 -0700

    Created collection of trivial arithmetic tests as well as a test initializeGgplot(), a non-exported function in granova.1w.ggplot().

---
+++ inst/tests/01.R
@@ -0,0 +1,10 @@
+test_that("one does not equal two", {
+  expect_that(1 == 2, is_false())
+  expect_that(1 == 2, is_true())
+  expect_that(1 == 1, is_true())
+  expect_that(1 == 1, is_false())
+})
+
+test_that("plot code produces ggplot objects", {
+  expect_that(initializeGgplot(), is_a("ggplot"))
+})
