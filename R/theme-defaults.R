67a56a5ad4cd7607e9116ad24839fb1a8637bb7b
Brian Danielak <briandk@umd.edu>
2011-05-29 21:59:06 -0400

    Changed theme_granova_1w and theme_granova_1w_gray to make sure that value labels on the x-axis are justified to the center of x-axis ticks.

--- R/theme-defaults.R
+++ R/theme-defaults.R
@@ -1,6 +1,6 @@
 theme_granova_1w <- function(base_size = 12) {
   theme_granova_1w                  <- theme_bw()
-  theme_granova_1w$axis.text.x      <- theme_text(size = 8, lineheight = 0.9, vjust = 1, angle = 90, colour = "grey50")
+  theme_granova_1w$axis.text.x      <- theme_text(size = 8, lineheight = 0.9, vjust = 0.5, angle = 90, colour = "grey50")
   theme_granova_1w$axis.text.y      <- theme_text(size = 8, lineheight = 0.9, hjust = 1, colour = "grey50")
   theme_granova_1w$axis.title.x     <- theme_text(size = 8, vjust = 2, colour = "grey20")
   theme_granova_1w$axis.title.y     <- theme_text(size = 8, angle = 90, vjust = 0.3, hjust = 0.5, colour = "grey20")
@@ -17,7 +17,7 @@ theme_granova_1w <- function(base_size = 12) {
   
 theme_granova_1w_gray <- function(base_size = 12) {
   theme_granova_1w_gray             <- theme_gray()
-  theme_granova_1w_gray$axis.text.x      <- theme_text(size = 8, lineheight = 0.9, vjust = 1, angle = 90, colour = "grey50")
+  theme_granova_1w_gray$axis.text.x      <- theme_text(size = 8, lineheight = 0.9, vjust = 0.5, angle = 90, colour = "grey50")
   theme_granova_1w_gray$axis.text.y      <- theme_text(size = 8, lineheight = 0.9, hjust = 1, colour = "grey50")
   theme_granova_1w_gray$axis.title.x     <- theme_text(size = 8, vjust = 2, colour = "grey20")
   theme_granova_1w_gray$axis.title.y     <- theme_text(size = 8, angle = 90, vjust = 0.3, hjust = 0.5, colour = "grey20")
