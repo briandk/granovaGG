79f1b845eade3f17970aae0f02b1ca51a9cb4d5a
Brian Danielak <briandk@umd.edu>
2011-07-12 17:09:30 -0400

    Commented out some granova.contr.ggplot demos

--- demo/granova.contr.ggplot.R
+++ demo/granova.contr.ggplot.R
@@ -3,19 +3,16 @@ contrasts22 <- data.frame( c(-.5,-.5,.5,.5),
   c(-.5,.5,-.5,.5), c(.5,-.5,-.5,.5) )
 names(contrasts22) <- c("Drug.A", "Drug.B", "Drug.A.B")
 
-granova.contr.ggplot(arousal, contrasts = contrasts22)
-granova.contr(arousal, contrasts = contrasts22)
-
-data(rat)
-dat6 <- matrix(c(1, 1, 1, -1, -1, -1, -1, 1, 0, -1, 1, 0, 1, 1, -2, 
-    1, 1, -2, -1, 1, 0, 1, -1, 0, 1, 1, -2, -1, -1, 2), ncol = 5)
-
-y <- granova.contr.ggplot(rat[,1], contrasts = dat6, ylab = "Rat Weight Gain", 
-  xlab = c("Amount 1 vs. Amount 2", "Type 1 vs. Type 2", 
-  "Type 1 & 2 vs Type 3", "Interaction of Amount and Type 1 & 2", 
-  "Interaction of Amount and  Type (1, 2), 3"))
-
-(y)
+granova.contr.ggplot(arousal, contrasts = contrasts22) -> gg
+# granova.contr(arousal, contrasts = contrasts22)
+# 
+# data(rat)
+# dat6 <- matrix(c(1, 1, 1, -1, -1, -1, -1, 1, 0, -1, 1, 0, 1, 1, -2, 
+#     1, 1, -2, -1, 1, 0, 1, -1, 0, 1, 1, -2, -1, -1, 2), ncol = 5)
+# 
+# y <- granova.contr.ggplot(rat[,1], contrasts = dat6)
+# 
+# (y)
 # 
 # #Polynomial Contrasts 
 # z <- granova.contr.ggplot(rat[,1],contrasts = contr.poly(6))
