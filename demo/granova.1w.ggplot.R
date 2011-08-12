a3dff68d15f6d2e493c1048c39111560553f98ad
Brian Danielak <briandk@umd.edu>
2011-08-12 16:06:19 -0400

    Commented out several demonstration examples in granova.1w.ggplot

--- demo/granova.1w.ggplot.R
+++ demo/granova.1w.ggplot.R
@@ -6,31 +6,31 @@ granova.1w.ggplot(arousal[,1:2])
 data(poison)
 ##Note violation of constant variance across groups in following graphic.
 granova.1w.ggplot(poison$SurvTime,     
-  group = poison$Group
+  group = poison$Group, resid=TRUE
 )
 
-granova.1w.ggplot(poison$RateSurvTime, 
-  group = poison$Group, 
-  resid = FALSE, 
-)
-
-granova.1w.ggplot(mtcars$hp, 
-  group = mtcars$cyl, 
-  resid = TRUE, 
-)
-
-granova.1w.ggplot(mtcars$hp, 
-  group      = mtcars$gear, 
-  resid      = TRUE, 
-  h.rng      = 20,
-  plot.theme = "theme_granova_1w",
-  main       = "test"
-)
-
-random.data <- data.frame(response = rnorm(n = 100), group = rep(1:10, each = 10))
-
-granova.1w.ggplot(random.data$response,
-  group = random.data$group,
-  resid = TRUE,
-  plot.theme = "theme_granova_1w"
-)
+# granova.1w.ggplot(poison$RateSurvTime, 
+#   group = poison$Group, 
+#   resid = FALSE, 
+# )
+# 
+# granova.1w.ggplot(mtcars$hp, 
+#   group = mtcars$cyl, 
+#   resid = TRUE, 
+# )
+# 
+# granova.1w.ggplot(mtcars$hp, 
+#   group      = mtcars$gear, 
+#   resid      = TRUE, 
+#   h.rng      = 20,
+#   plot.theme = "theme_granova_1w",
+#   main       = "test"
+# )
+# 
+# random.data <- data.frame(response = rnorm(n = 100), group = rep(1:10, each = 10))
+# 
+# granova.1w.ggplot(random.data$response,
+#   group = random.data$group,
+#   resid = TRUE,
+#   plot.theme = "theme_granova_1w"
+# )
