410e16bc996b494e45f6120b3cb9b0c655b114e1
Brian Danielak <briandk@umd.edu>
2011-08-19 19:23:58 -0400

    Replaced calls to granova.ds with calls to granovagg.ds in granovagg.ds demo

--- demo/granovagg.ds.R
+++ demo/granovagg.ds.R
@@ -1,11 +1,11 @@
 ### See discussion of anorexia graphic in EDSAG, J. Statistics Ed.
 data(anorexia.sub)
 
-granova.ds(anorexia.sub, revc = TRUE, 
+granovagg.ds(anorexia.sub, revc = TRUE, 
   main = "Assessment Plot for weights to assess Family Therapy treatment 
   for Anorexia Patients")
 # If labels for four unusual points at lower left are desired:
-granova.ds(anorexia.sub, revc = TRUE, 
+granovagg.ds(anorexia.sub, revc = TRUE, 
   main = "Assessment Plot for weights to assess Family Therapy treatment 
   for Anorexia Patients", ident = TRUE)
 
@@ -13,6 +13,6 @@ granova.ds(anorexia.sub, revc = TRUE,
 ## See discussion of blood lead graphic in EDSAG, J. Statistics Ed.
 data(blood_lead)
 
-granova.ds(blood_lead, sw = .1, 
+granovagg.ds(blood_lead, sw = .1, 
    main = "Dependent Sample Assessment Plot
    Blood Lead Levels of Matched Pairs of Children")
