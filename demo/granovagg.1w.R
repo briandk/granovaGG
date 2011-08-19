60aab2cefef0fc764c7a419a45d925927867cc5a
Brian Danielak <briandk@umd.edu>
2011-08-19 18:28:31 -0400

    Replaced granovagg.1w demo code with example from granova.1w

--- demo/granovagg.1w.R
+++ demo/granovagg.1w.R
@@ -1,36 +1,26 @@
 data(arousal)
 #Drug A
-granovagg.1w(arousal[,1:2], group = arousal[,2])
-granovagg.1w(arousal[,1:2])
+granova.1w(arousal[,1:2], h.rng = 1.6, v.rng = 0.5, top.dot = .35)
+
+#########################
+
+library(MASS)
+wt.gain <- anorexia[, 3] - anorexia[, 2]
+granova.1w(wt.gain, group = anorexia[, 1], size.line = -3)
+
+##########################
 
 data(poison)
 ##Note violation of constant variance across groups in following graphic.
-granovagg.1w(poison$SurvTime,     
-  group = poison$Group, resid=TRUE
-)
+granova.1w(poison$SurvTime, group = poison$Group, ylab = "Survival Time")
+##RateSurvTime = SurvTime^-1
+granova.1w(poison$RateSurvTime, group = poison$Group, 
+ylab = "Survival Rate = Inverse of Survival Time")
 
-# granovagg.1w(poison$RateSurvTime, 
-#   group = poison$Group, 
-#   resid = FALSE, 
-# )
-# 
-# granovagg.1w(mtcars$hp, 
-#   group = mtcars$cyl, 
-#   resid = TRUE, 
-# )
-# 
-# granovagg.1w(mtcars$hp, 
-#   group      = mtcars$gear, 
-#   resid      = TRUE, 
-#   h.rng      = 20,
-#   plot.theme = "theme_granova_1w",
-#   main       = "test"
-# )
-# 
-# random.data <- data.frame(response = rnorm(n = 100), group = rep(1:10, each = 10))
-# 
-# granovagg.1w(random.data$response,
-#   group = random.data$group,
-#   resid = TRUE,
-#   plot.theme = "theme_granova_1w"
-# )
+##Nonparametric version: RateSurvTime ranked and rescaled
+##to be comparable to RateSurvTime; 
+##note labels as well as residual (rug) plot below.
+granova.1w(poison$RankRateSurvTime, group = poison$Group, 
+ylab = "Ranked and Centered Survival Rates",
+main = "One-way ANOVA display, poison data (ignoring 2-way set-up)", 
+res = TRUE)
