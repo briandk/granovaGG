d2405068c04f8f94a0a59471c882fcce11524241
Brian Danielak <briandk@umd.edu>
2011-08-19 19:12:34 -0400

    Replaced granovagg.contr demo code with example code from granova.contr

--- demo/granovagg.contr.R
+++ demo/granovagg.contr.R
@@ -2,28 +2,19 @@ data(arousal)
 contrasts22 <- data.frame( c(-.5,-.5,.5,.5), 
   c(-.5,.5,-.5,.5), c(.5,-.5,-.5,.5) )
 names(contrasts22) <- c("Drug.A", "Drug.B", "Drug.A.B")
-
-granovagg.contr(arousal, contrasts = contrasts22, print.four.plots.per.page = TRUE, ylab = "Test 1")
 granova.contr(arousal, contrasts = contrasts22)
-# granova.contr(arousal, contrasts = contrasts22)
-# 
-# data(rat)
-# dat6 <- matrix(c(1, 1, 1, -1, -1, -1, -1, 1, 0, -1, 1, 0, 1, 1, -2, 
-#     1, 1, -2, -1, 1, 0, 1, -1, 0, 1, 1, -2, -1, -1, 2), ncol = 5)
-# 
-# y <- granovagg.contr(rat[,1], contrasts = dat6)
-# 
-# (y)
-# 
-#Polynomial Contrasts 
+  
 data(rat)
-granova.contr(rat[,1], contrasts = contr.poly(6))
-granovagg.contr(rat[,1], contrasts = contr.poly(6))
+dat6 <- matrix(c(1, 1, 1, -1, -1, -1, -1, 1, 0, -1, 1, 0, 1, 1, -2, 
+    1, 1, -2, -1, 1, 0, 1, -1, 0, 1, 1, -2, -1, -1, 2), ncol = 5)
+granova.contr(rat[,1], contrasts = dat6, ylab = "Rat Weight Gain", 
+  xlab = c("Amount 1 vs. Amount 2", "Type 1 vs. Type 2", 
+  "Type 1 & 2 vs Type 3", "Interaction of Amount and Type 1 & 2", 
+  "Interaction of Amount and  Type (1, 2), 3"))
+#Polynomial Contrasts 
+granova.contr(rat[,1],contrasts = contr.poly(6))
 
-# 
-# #based on random data 
-# data.random <- rt(64, 5)
-# LayoutFourPlotsPerPage(
-#   granovagg.contr(data.random, contrasts = contr.helmert(8), 
-#   ylab = "Random Data")
-# )
+#based on random data 
+data.random <- rt(64, 5)
+granova.contr(data.random, contrasts = contr.helmert(8), 
+  ylab = "Random Data")
