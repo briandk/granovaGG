a8fd35dfafe9dc9587e4574bb89dce1dcf1e8172
Brian Danielak <briandk@umd.edu>
2011-08-19 18:06:07 -0400

    Anorexia data documentation is now generated from a roxygen block in granovaGG.R

--- R/granovaGG.R
+++ R/granovaGG.R
@@ -52,6 +52,33 @@
 #' \code{\link{granovagg.1w}} \code{\link{granovagg.ds}}
 #'   \code{\link{granovagg.contr}}
 #' @keywords hplot
+NULL
 
+#' Family Treatment Weight change data for young female anorexia patients.
+#' 
 
+#' 
+#' The MASS package includes the dataset \code{anorexia}, containing pre and
+#' post treatment weights for young female anorexia patients.  This is a subset
+#' of those data, containing only those patients who received Family Treatment.
+#' 
+#' 
+#' @name anorexia.sub
+#' @docType data
+#' @format A dataframe with 17 observations on the following 2 variables, no
+#'   NAs.
+#' 
+#' \describe{
+#' 
+#' \item{list("Prewt")}{Pretreatment weight of subject, in pounds.}
+#' 
+#' \item{list("Postwt")}{Postreatment weight of subject, in pounds.}
+#' 
+#' }
+#' @references Venables, W. N. and Ripley, B. D. (2002) Modern Applied
+#'   Statistics with S. Fourth edition. Springer.
+#' @source Hand, D. J., Daly, F., McConway, K., Lunn, D. and Ostrowski, E. eds
+#'   (1993) A Handbook of Small Data Sets. Chapman & Hall, Data set 285 (p.
+#'   229)
+#' @keywords datasets
 NULL
