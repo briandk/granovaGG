1d45320926b93d746f5bd9f1ac5ea4a0ddc87eef
Brian Danielak <briandk@umd.edu>
2012-07-16 18:14:32 -0400

    Move InitializeGgplot_1w to helper file
    Fix indenting on 1w scale functions

--- R/granovagg.1w-helpers.R
+++ R/granovagg.1w-helpers.R
@@ -1,6 +1,6 @@
 IsFSignificant <- function(model.summary) {
-  f.critical  <- qf(p   = 0.95, 
-                    df1 = model.summary$fstatistic["numdf"], 
+  f.critical  <- qf(p   = 0.95,
+                    df1 = model.summary$fstatistic["numdf"],
                     df2 = model.summary$fstatistic["dendf"]
                  )
   return(as.logical(
@@ -10,26 +10,29 @@ IsFSignificant <- function(model.summary) {
 }
 
 ## Plotting Functions
+InitializeGgplot_1w <- function() {
+  return(ggplot())
+}
 
-  ScaleX_1w <- function(owp) {
-    return(
-      scale_x_continuous(
-        breaks = (owp$params$aggregate.x.breaks),
-        labels = signif(owp$params$aggregate.x.breaks, digits = 2),
-        limits = owp$params$x.range,
-        expand = c(0.00, 0)
-      )
+ScaleX_1w <- function(owp) {
+  return(
+    scale_x_continuous(
+      breaks = (owp$params$aggregate.x.breaks),
+      labels = signif(owp$params$aggregate.x.breaks, digits = 2),
+      limits = owp$params$x.range,
+      expand = c(0.00, 0)
     )
-  }
+  )
+}
 
-  ScaleY_1w <- function(owp) {
-    return(
-      scale_y_continuous(
-        breaks = (owp$params$aggregate.y.breaks),
-        labels = signif(owp$params$aggregate.y.breaks, digits = 2),
-        limits = owp$params$y.range,
-        expand = c(0.00, 0)
-      )
+ScaleY_1w <- function(owp) {
+  return(
+    scale_y_continuous(
+      breaks = (owp$params$aggregate.y.breaks),
+      labels = signif(owp$params$aggregate.y.breaks, digits = 2),
+      limits = owp$params$y.range,
+      expand = c(0.00, 0)
     )
-  }
+  )
+}
 
