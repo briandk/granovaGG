8580c1c0843d65e8171f7c0a2b951d9e136416db
Brian Danielak <brian@briandk.com>
2014-12-31 14:39:39 -0500

    Unsuccessfully try to appease R CMD check
    and its “no binding defined for” variable code check warning

--- R/granovagg.1w.R
+++ R/granovagg.1w.R
@@ -121,6 +121,40 @@ granovagg.1w <- function(data,
                 )
 
 {
+  appeaseRCmdCheck <- function() {
+    # The sole purpose of this function is to avoid
+    # "no visible binding for [variable] NOTEs in
+    # R cmd check
+    nonVisibleBindings <- c("ymin",
+                            "fill",
+                            "xmax",
+                            "angle",
+                            "maximum.score",
+                            "yend",
+                            "baseline.variation",
+                            "label",
+                            "xmin",
+                            "within.1.sd.of.the.mean.of.all.residuals",
+                            "score",
+                            "group.mean",
+                            "within.group.residuals",
+                            "ylabel",
+                            "y",
+                            "x",
+                            "xend",
+                            "overplotted",
+                            "contrast",
+                            "ymax"
+    )
+    globalVariables(names)
+
+#     for (variableName in nonVisibleBindings) {
+#       assign(x = variableName,
+#              value = NULL)
+#     }
+
+  }
+
   yy <- data
 
   CoerceHigherDimensionalDataToMatrix <- function(data) {
@@ -939,6 +973,9 @@ granovagg.1w <- function(data,
       )
     )
   }
+
+  # Appease R CMD Check
+  appeaseRCmdCheck()
 
   # Pepare OWP object
   owp                       <- AdaptVariablesFromGranovaComputations()
